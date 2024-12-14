b489975ad4145e0687601df34ece33e0ea973ec3
harshfreikin <harshtiwari8880@gmail.com>
2024-12-15 00:44:24 +0530

    tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Add the correct paths here
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
