b969bfa299a9e6b13be5cd1fa1763e96ff8466fb
harshfreikin <harshtiwari8880@gmail.com>
2024-12-15 00:52:19 +0530

    name and package

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,7 +8,7 @@ const Education = () => {
         initial={{ opacity: 0, y: -20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }} 
-        className="mb-4 text-center text-3xl font-bold">Eductaion</motion.h2>
+        className="mb-4 text-center text-3xl font-bold">Education</motion.h2>
         {EDUCATION.map((edu,index)=>(
             <motion.div
             initial={{ opacity: 0, x: -20  }}
